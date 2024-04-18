ff21711b60dca8e96b4e92f74fbc6819820a86e9
kioharry0 <kio.harry1@gmail.com>
2024-04-18 08:58:45 -0700

    script

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let Thefirstline= "C is fun";
+let Thesecondline= "Python is cool";
+let Thethirdline= 'JavaScript is amazing';
+
+console.log(Thefirstline);
+console.log(Thesecondline);
+console.log(Thethirdline);
